0a4e1292e3521c478a3b63e8d8849d53992b119d
Thanh Tran <tt222kj@student.lnu.se>
2022-05-23 16:34:28 +0200

    set up express

---
+++ src/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { router } from "./routes/router.js";
+
+// The main function of the application.
+const main = async () => {
+  const app = express();
+  const directoryFullName = dirname(fileURLToPath(import.meta.url));
+  // To allow cross origin connections so that the webapp can connect to our server
+  app.use(cors());
+
+  if (process.env.NODE_ENV === 'production') {
+    // Serve static files.
+    app.use(express.static(join(directoryFullName, 'public')))
+
+    app.get('/', (req, res) => {
+      res.sendFile(join(directoryFullName + './public/index.html'))
+    })
+  }
+
+  // Register routes.
+  app.use("/", router);
+
+  app.listen(process.env.PORT, () => {
+    console.log(`App is running at ${process.env.PORT}`);
+    console.log("Press Ctrl-C to terminate...");
+  });
+};
+
+main().catch(console.error);
