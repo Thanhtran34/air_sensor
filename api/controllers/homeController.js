c9cd6c675f2885665bd9c42f0bce52ceb57cd18c
Thanh Tran <tt222kj@student.lnu.se>
2022-05-25 22:53:04 +0200

    try pusher

--- api/controllers/homeController.js
+++ api/controllers/homeController.js
@@ -3,6 +3,7 @@
  */
  import admin from "firebase-admin";
  import { cert } from "firebase-admin/app";
+ import Pusher from "pusher";
  
  admin.initializeApp({
    credential: cert({
@@ -20,8 +21,19 @@
    databaseURL: process.env.DATABASE,
  });
  const db = admin.database();
- const path = "/UsersData/" + process.env.SECRET + "/readings"
+ const path = "/UsersData/" + process.env.SECRET
  const userRef = db.ref(path);
+ const data = [];
+ let sensorData;
+ let newSensorData;
+
+ const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_APP_KEY,
+  secret: process.env.PUSHER_APP_SECRET,
+  cluster: process.env.PUSHER_APP_CLUSTER,
+  encrypted: true,
+});
  
  /**
   * Encapsulates a home controller.
@@ -29,9 +41,32 @@
  export class HomeController {
    async getSensorData(req, res, next) {
      try {
-       await userRef.once("value", (snap) => {
-         res.status(200).json(snap.val());
+       await userRef
+       .child("readings")
+       .limitToLast(5)
+       .once("value", (snap) => {
+         sensorData = snap.val();
        });
+
+       Object.values(sensorData).forEach(val => {
+         data.push(val)
+       })
+
+       res.status(200).json(sensorData);
+     } catch (e) {
+       next(e);
+     }
+   }
+
+   async getNewerData (req, res, next) {
+     try {
+      await userRef
+      .child("readings")
+      .limitToLast(1)
+      .once("value", (snap) => {
+        newSensorData = snap.val();
+      });
+      data.push(newSensorData);
      } catch (e) {
        next(e);
      }
