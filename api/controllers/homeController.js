bda6b1e55b86fc220e8b6745f2c737d562b9033e
Thanh Tran <tt222kj@student.lnu.se>
2022-05-28 17:40:34 +0200

    try vue chart js

--- api/controllers/homeController.js
+++ api/controllers/homeController.js
@@ -1,75 +1,159 @@
 /**
  * Home controller
  */
- import admin from "firebase-admin";
- import { cert } from "firebase-admin/app";
- import Pusher from "pusher";
- 
- admin.initializeApp({
-   credential: cert({
-     type: process.env.TYPE,
-     project_id: process.env.ID,
-     private_key_id: process.env.KEY_ID,
-     private_key: process.env.KEY,
-     client_email:process.env.EMAIL,
-     client_id: process.env.CLIENT,
-     auth_uri: process.env.AUTH_URI,
-     token_uri: process.env.TOKEN_URI,
-     auth_provider_x509_cert_url: process.env.AUTH_PROVIDER,
-     client_x509_cert_url: process.env.CLIENT_CERT,
-   }),
-   databaseURL: process.env.DATABASE,
- });
- const db = admin.database();
- const path = "/UsersData/" + process.env.SECRET
- const userRef = db.ref(path);
- const data = [];
- let sensorData;
- let newSensorData;
+import admin from "firebase-admin";
+import { cert } from "firebase-admin/app";
+import Pusher from "pusher";
 
- const pusher = new Pusher({
+admin.initializeApp({
+  credential: cert({
+    type: process.env.TYPE,
+    project_id: process.env.ID,
+    private_key_id: process.env.KEY_ID,
+    private_key: process.env.KEY,
+    client_email: process.env.EMAIL,
+    client_id: process.env.CLIENT,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER,
+    client_x509_cert_url: process.env.CLIENT_CERT,
+  }),
+  databaseURL: process.env.DATABASE,
+});
+const db = admin.database();
+const path = "/UsersData/" + process.env.SECRET;
+const userRef = db.ref(path);
+const data = [];
+const humiditySensor = [];
+const temperatureSensor = [];
+const gasSensor = [];
+let humidityData;
+let temperatureData;
+let gasData;
+let sensor;
+let newSensorData;
+
+const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID,
   key: process.env.PUSHER_APP_KEY,
   secret: process.env.PUSHER_APP_SECRET,
   cluster: process.env.PUSHER_APP_CLUSTER,
   encrypted: true,
 });
- 
- /**
-  * Encapsulates a home controller.
-  */
- export class HomeController {
-   async getSensorData(req, res, next) {
-     try {
-       await userRef
-       .child("readings")
-       .limitToLast(5)
-       .once("value", (snap) => {
-         sensorData = snap.val();
-       });
 
-       Object.values(sensorData).forEach(val => {
-         data.push(val)
-       })
-
-       res.status(200).json(sensorData);
-     } catch (e) {
-       next(e);
-     }
-   }
-
-   async getNewerData (req, res, next) {
-     try {
+/**
+ * Encapsulates a home controller.
+ */
+export class HomeController {
+  async getHumidity(req, res, next) {
+    try {
       await userRef
-      .child("readings")
-      .limitToLast(1)
-      .once("value", (snap) => {
-        newSensorData = snap.val();
+        .child("readings")
+        .limitToLast(6)
+        .once("value", (snap) => {
+          humidityData = snap.val();
+        });
+
+      Object.values(humidityData).forEach((val) => {
+        humiditySensor.push({
+          humidity: val.humidity,
+          timestamp: val.timestamp,
+        });
       });
-      data.push(newSensorData);
-     } catch (e) {
-       next(e);
-     }
-   }
- }
- 
+      res.status(200).json({ hRecords: humiditySensor });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getTemperature(req, res, next) {
+    try {
+      await userRef
+        .child("readings")
+        .limitToLast(6)
+        .once("value", (snap) => {
+          temperatureData = snap.val();
+        });
+
+      Object.values(temperatureData).forEach((val) => {
+        temperatureSensor.push({
+          temperature: val.temperature,
+          timestamp: val.timestamp,
+        });
+      });
+      res.status(200).json({ tRecords: temperatureSensor });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getHumAndTem(req, res, next) {
+    try {
+      await userRef
+        .child("readings")
+        .limitToLast(6)
+        .once("value", (snap) => {
+          sensor = snap.val();
+        });
+
+      Object.values(sensor).forEach((val) => {
+        data.push({
+          humidity: val.humidity,
+          temperature: val.temperature,
+          timestamp: val.timestamp,
+        });
+      });
+      res.status(200).json({ dataPoints: data });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async getGas(req, res, next) {
+    try {
+      await userRef
+        .child("readings")
+        .limitToLast(6)
+        .once("value", (snap) => {
+          gasData = snap.val();
+        });
+
+      Object.values(gasData).forEach((val) => {
+        gasSensor.push({
+          gas: val.gas,
+          timestamp: val.timestamp,
+        });
+      });
+      res.status(200).json({ gRecords: gasSensor });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  updateData(req, res, next) {
+    try {
+      let newPoint;
+      setInterval(async () => {
+        await userRef
+          .child("readings")
+          .limitToLast(1)
+          .once("value", (snap) => {
+            newSensorData = snap.val();
+          });
+
+        newPoint = Object.values(newSensorData).pop();
+
+        data.push(newPoint);
+        pusher.trigger("air-quality", "new-quality", {
+          dataPoint: newPoint,
+        });
+      }, 2000);
+      res.send({
+        success: true,
+        dataPoint: newPoint
+      });
+    } catch (e) {
+      next(e);
+    }
+  }
+}
--- api/routes/router.js
+++ api/routes/router.js
@@ -7,7 +7,16 @@
  export const router = express.Router();
  const controller = new HomeController();
  
- router.get("/api", (req, res, next) => controller.getSensorData(req, res, next));
+ router.get("/api/quality", (req, res, next) => {
+  controller.getHumAndTem(req, res, next)
+  //controller.updateData(req, res, next)
+ });
+ router.get("/api/humidity", (req, res, next) => controller.getHumidity(req, res, next));
+ router.get("/api/temperature", (req, res, next) => controller.getTemperature(req, res, next));
+ router.get("/api/gas", (req, res, next) => controller.getGas(req, res, next));
+ router.get("/api/update", (req, res, next) => controller.updateData(req, res, next));
+
+
  router.use("*", (req, res, next) => {
    const error = new Error();
    error.status = 404;
