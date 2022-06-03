8b0059a98ba45df03e970c7c6cdba95fbe5741e2
Thanh Tran <tt222kj@student.lnu.se>
2022-06-03 16:00:05 +0200

    clean code

--- api/server.js
+++ api/server.js
@@ -11,17 +11,6 @@ const main = async () => {
   // To allow cross origin connections so that the webapp can connect to our server
   app.use(cors());
 
-  /** 
-  if (process.env.NODE_ENV === 'production') {
-    // Serve static files.
-    app.use(express.static(join(directoryFullName, 'public')))
-
-    app.get("/", (req, res) => {
-      res.sendFile(join(directoryFullName + './public/index.html'))
-    })
-  }
-  */
-
   // Register routes.
   app.use("/", router);
 
